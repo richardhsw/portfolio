6548a5ead9753eab80f87e41bcd157df0fa1225c
Richard Hsu <16552744+richardhsw@users.noreply.github.com>
2020-07-29 12:18:59 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3c28d486dc23eeec90d0b903951e30c2.js"
+  "/portfolio/precache-manifest.a278aaf5246f05e3172cf52b729d41a9.js"
 );
 
 self.addEventListener('message', (event) => {
