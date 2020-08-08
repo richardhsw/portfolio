af53054c9a2631c9d903225e340032537b92e80e
Richard Hsu <16552744+richardhsw@users.noreply.github.com>
2020-08-07 17:09:14 -0700

    Fixed image not appearing after refresh issue

--- src/components/elements/Image.js
+++ src/components/elements/Image.js
@@ -29,33 +29,33 @@ const Image = ({
 
   const image = useRef(null);
 
-  useEffect(() => {
-    const placeholderImage = document.createElement('img');
-    handlePlaceholder(image.current, placeholderImage);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   const placeholderImage = document.createElement('img');
+  //   handlePlaceholder(image.current, placeholderImage);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
-  const placeholderSrc = (w, h) => {
-    return `data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${w} ${h}"%3E%3C/svg%3E`;
-  }
+  // const placeholderSrc = (w, h) => {
+  //   return `data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${w} ${h}"%3E%3C/svg%3E`;
+  // }
 
-  const handlePlaceholder = (img, placeholder) => {
-    img.style.display = 'none';
-    img.before(placeholder);
-    placeholder.src = placeholderSrc(
-      img.getAttribute('width') || 0,
-      img.getAttribute('height') || 0
-    );
-    placeholder.width = img.getAttribute('width');
-    placeholder.height = img.getAttribute('height');
-    placeholder.style.opacity = '0';
-    img.className && placeholder.classList.add(img.className);
+  // const handlePlaceholder = (img, placeholder) => {
+  //   img.style.display = 'none';
+  //   img.before(placeholder);
+  //   placeholder.src = placeholderSrc(
+  //     img.getAttribute('width') || 0,
+  //     img.getAttribute('height') || 0
+  //   );
+  //   placeholder.width = img.getAttribute('width');
+  //   placeholder.height = img.getAttribute('height');
+  //   placeholder.style.opacity = '0';
+  //   img.className && placeholder.classList.add(img.className);
 
-    img.addEventListener('load', () => {
-      placeholder.remove();
-      img.style.display = '';
-    });
-  }
+  //   img.addEventListener('load', () => {
+  //     placeholder.remove();
+  //     img.style.display = '';
+  //   });
+  // }
 
   return (
     <img
