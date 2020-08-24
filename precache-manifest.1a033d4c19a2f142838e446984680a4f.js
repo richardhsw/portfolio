d01c88c5040c6825d7f364f41cb98297718bb3a3
Richard Hsu <16552744+richardhsw@users.noreply.github.com>
2020-08-24 14:21:39 -0700

    Updates

---
+++ precache-manifest.1a033d4c19a2f142838e446984680a4f.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd3f0e287c23b157fca1b5d235850df",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ab05b95ae79239944850",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0c2c0ddab5f46e9d3f32",
+    "url": "/portfolio/static/css/main.db7982d5.chunk.css"
+  },
+  {
+    "revision": "ab05b95ae79239944850",
+    "url": "/portfolio/static/js/2.f3a20cbb.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/portfolio/static/js/2.f3a20cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2c0ddab5f46e9d3f32",
+    "url": "/portfolio/static/js/main.a01d849c.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "12cadc681268ec221179bccf2972a707",
+    "url": "/portfolio/static/media/GOTOLocker.12cadc68.png"
+  },
+  {
+    "revision": "12eb916d86860a85fdaf50a7b65a3fb9",
+    "url": "/portfolio/static/media/Resume.12eb916d.pdf"
+  },
+  {
+    "revision": "28f766d5dcee926901712b4ea9caac89",
+    "url": "/portfolio/static/media/SugoifridgeTriple.28f766d5.png"
+  },
+  {
+    "revision": "eee6cf1a79e0dcf5660bff45266cf2a5",
+    "url": "/portfolio/static/media/Where2Meet.eee6cf1a.png"
+  },
+  {
+    "revision": "0cc4bc06de43e4d787f7858cd51f966b",
+    "url": "/portfolio/static/media/cpp.0cc4bc06.svg"
+  },
+  {
+    "revision": "2b51d9abb7663860affcc7b2afb8e1f4",
+    "url": "/portfolio/static/media/csharp.2b51d9ab.svg"
+  },
+  {
+    "revision": "f2fff6f13800ca8d71cdb0a4bb50ab9a",
+    "url": "/portfolio/static/media/css.f2fff6f1.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/portfolio/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "83f6710b31431efdb88252c79ee72ba2",
+    "url": "/portfolio/static/media/github.83f6710b.svg"
+  },
+  {
+    "revision": "84894ab27a86cbcb7d8961f8b7680ea9",
+    "url": "/portfolio/static/media/html.84894ab2.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/portfolio/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/portfolio/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "00d6a3837267cea56a8880ee483e8ac8",
+    "url": "/portfolio/static/media/java.00d6a383.svg"
+  },
+  {
+    "revision": "08f445b66028b41533d8f003de54aa2b",
+    "url": "/portfolio/static/media/javascript.08f445b6.svg"
+  },
+  {
+    "revision": "1e506fde4c22957fe9506687d2fdb45e",
+    "url": "/portfolio/static/media/linkedin.1e506fde.svg"
+  },
+  {
+    "revision": "a22651a7085d2be3de4b0380c3554560",
+    "url": "/portfolio/static/media/logo.a22651a7.svg"
+  },
+  {
+    "revision": "a4a906edea20a10ac27f38b1574f8408",
+    "url": "/portfolio/static/media/mail.a4a906ed.svg"
+  },
+  {
+    "revision": "7c4f9c84cbbee86f327042d6264d0384",
+    "url": "/portfolio/static/media/mysql.7c4f9c84.svg"
+  },
+  {
+    "revision": "fbef321e03c16bcae42560f8507757d6",
+    "url": "/portfolio/static/media/python.fbef321e.svg"
+  },
+  {
+    "revision": "7ddd8191ee4d64f26683886ab799a499",
+    "url": "/portfolio/static/media/react.7ddd8191.svg"
+  },
+  {
+    "revision": "23184a95d92bd6cda91a7df13935f82a",
+    "url": "/portfolio/static/media/swift.23184a95.svg"
+  },
+  {
+    "revision": "5961fdaec27a32099984d237fb63f945",
+    "url": "/portfolio/static/media/tux.5961fdae.svg"
+  },
+  {
+    "revision": "c6e2e79dc7e0d1f2949675eed0295062",
+    "url": "/portfolio/static/media/visualbasic.c6e2e79d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0d98aedd7e12cef6a9cad1f7767c65f2.js"
+  "/portfolio/precache-manifest.1a033d4c19a2f142838e446984680a4f.js"
 );
 
 self.addEventListener('message', (event) => {
